3d9fce4914539f831b7046e883e5ee434388d075
editor-taka <takachan1050150@gmail.com>
2023-07-25 10:49:17 -0400

    show default page

--- index.js
+++ index.js
@@ -21,31 +21,60 @@ const fs = require('fs');
 const users = require('./resources/data/users.json');
 
 const server = http.createServer((req, res) =>{
-    if(req.url === '/'){
-        fs.readFile(path.join(__dirname, 'public', 'index.html'),
-        (error, content)=>{
-            if(error){
-                throw error;
+    // if(req.url === '/api/users'){
+    //     res.writeHead(200, {"Content-Type": 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+    
+    // Build file Path
+    let filePath = path.join(
+        __dirname, 'public',
+        req.url === '/' ? 'index.html' : req.url
+    );
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+    fs.readFile(filePath, (err, content)=>{
+        if(err){
+            if(err.code == 'ENOENT'){
+                // Page Not Found
+                fs.readFile(path.join(__dirname, 'public', '404.html'),
+                (err, content)=>{
+                    res.writeHead(200, {'Content-Type':'text/html'});
+                    res.end(content, 'utf-8');
+                })
             }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    }
-    else if(req.url === '/about'){
-        fs.readFile(path.join(__dirname, 'public', 'about.html'),
-        (error, content)=>{
-            if(error){
-                throw error;
+            else{
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}.`);
             }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    }
-    else if(req.url === '/api/users'){
-        res.writeHead(200, {"Content-Type": 'application/json'});
-        res.end(JSON.stringify(users));
-    }
-    console.log(req.url);
+        }
+        else{
+            // Success
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf-8');
+        }
+    })
 });
 
 const PORT = process.env.PORT || 5000;
