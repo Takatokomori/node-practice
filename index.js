ff67cd3c5e243831dec50d5bbf0ae4c14a663248
editor-taka <takachan1050150@gmail.com>
2023-07-25 10:15:31 -0400

    demo http practice

--- index.js
+++ index.js
@@ -15,3 +15,40 @@
 
 // logger.log('Hello World');
 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const users = require('./resources/data/users.json');
+
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+        (error, content)=>{
+            if(error){
+                throw error;
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
+    else if(req.url === '/about'){
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),
+        (error, content)=>{
+            if(error){
+                throw error;
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
+    else if(req.url === '/api/users'){
+        res.writeHead(200, {"Content-Type": 'application/json'});
+        res.end(JSON.stringify(users));
+    }
+    console.log(req.url);
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
