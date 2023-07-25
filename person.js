431abdae6b7d53d6cc1edcf0314f2dd55ba08345
editor-taka <takachan1050150@gmail.com>
2023-07-25 09:38:22 -0400

    delete contents for http

--- person.js
+++ person.js
@@ -9,4 +9,11 @@ class Person {
     }
 }
 
-module.exports = Person;
+// module.exports = Person;
+
+const person1 = new Person('Josh, Ming', 44);
+const person2 = new Person('Bobby Gu', 33);
+
+console.log(person1);
+console.log(person2);
+person2.greeting();
