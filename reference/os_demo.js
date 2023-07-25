94cd08cbe61d4dbce239013258001ce1f3c0f51b
editor-taka <takachan1050150@gmail.com>
2023-07-25 08:18:33 -0400

    os module

---
+++ reference/os_demo.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.cpus());
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.hostname());
+console.log(os.homedir());
